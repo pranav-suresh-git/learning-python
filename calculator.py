6ca56f59e64efb287b52fd885c0f02cfdd63bee4
Pranav Suresh <pranavsuresh@gmail.com>
2025-05-21 00:56:08 -0500

    initial commit: Day 1 files

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#calculator.py
+
+def add(x, y): return x + y
+def substract(x,y): return x - y
+def multiply(x,y): return x *y
+
+print (add(3, 4)) # output : 7
---
+++ variables.py
@@ -0,0 +1,17 @@
+## variables.py
+
+# integer
+
+age = 21
+
+# Float
+
+pi = 3.14159
+
+# String
+name = "Pranav"
+
+#Print them out
+
+print(f"Name: {name}, Age: {age}, Pi: {pi}:")
+
